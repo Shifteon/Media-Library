9cecee25933113aeb6bc05bc29fe650f0dad98f4
Shifteon <bpwyatt04@gmail.com>
2021-06-08 13:56:19 -0600

    Added functionality for cds. Created displayLibrary() method.

--- Library.kt
+++ Library.kt
@@ -1,6 +1,7 @@
 class Library {
     var books: MutableList<Book> = mutableListOf();
     var movies: MutableList<Movie> = mutableListOf();
+    var cds: MutableList<CD> = mutableListOf();
 
     /**
      * Add a book
@@ -14,6 +15,13 @@ class Library {
      */
     fun addMovie(movie: Movie) {
         movies.add(movie);
+    }
+
+    /**
+     * Add a cd
+     */
+    fun addCD(cd: CD) {
+        cds.add(cd);
     }
 
     /**
@@ -45,6 +53,59 @@ class Library {
                 println(movie);
                 println("---------------------");
             }
+        }
+    }
+
+    /**
+     * Displays all the cds
+     */
+    fun displayCDs() {
+        println("==========CDs=========");
+        // Check if they have any cds
+        if (cds.isEmpty()) {
+            println("You have no CDs!");
+        } else {
+            for (cd in cds) {
+                println(cd);
+                println("---------------------");
+            }
+        }
+    }
+
+    /**
+     * Displays the title of everything in the library
+     */
+    fun displayLibrary() {
+        // What's the greatest number of things we'll have to loop through?
+        val num: Int = when {
+            books.size > movies.size && books.size > cds.size -> books.size;
+            movies.size > books.size && movies.size > cds.size -> movies.size;
+            else -> cds.size;
+        }
+
+        // Print
+        println("========Books=========\t========Movies========\t==========CDs=========");
+        for (i in 0 until num) {
+            // Are we outside of the lists range?
+            var bookTitle: String =  if (books.size - 1 >= i) books[i].title else " ";
+            var movieTitle: String = if (movies.size - 1 >= i) movies[i].title else " ";
+            var cdTitle: String =    if (cds.size - 1 >= i) cds[i].title else " ";
+
+            // Create spaces so things look nice
+            if (bookTitle.length < 21) {
+                while (bookTitle.length < 21) {
+                    bookTitle += " ";
+                }
+            }
+            if (movieTitle.length < 21) {
+                while (movieTitle.length < 21) {
+                    movieTitle += " ";
+                }
+            }
+
+            // Print everything out
+            println("$bookTitle\t$movieTitle\t$cdTitle");
+            println("---------------------\t---------------------\t---------------------");
         }
     }
 }
